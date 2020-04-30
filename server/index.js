4bcec41b0d272da791e667b007f759454c1c7770
juliannawilamowska <juliawilamowska@gmail.com>
2020-04-30 17:40:37 +0200

    Dodano ikone

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const path = require('path')
 const Koa = require('koa')
 const serve = require('koa-static')
 const logger = require('koa-logger')
+const favicon = require('koa-favicon')
 const app = new Koa()
 const port = process.env.PORT || 3000
 
+app.use(favicon('./client/favicon.ico'))
 app.use(logger())
 
 app.use(serve(path.resolve(__dirname, '..', 'client')))
